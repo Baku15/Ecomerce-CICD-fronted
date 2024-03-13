15acfed1a47b979354be1556b2d2a71b00fb0d0f
freddy_chura <freddychura93@gmail.com>
2024-03-13 13:06:53 -0400

    Generando componentes listado y filtrado de productos  por categoria

---
+++ src/app/categoria-producto/categoria-producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-producto',
+  standalone: true,
+  imports: [],
+  templateUrl: './categoria-producto.component.html',
+  styleUrl: './categoria-producto.component.css'
+})
+export class CategoriaProductoComponent {
+
+}
