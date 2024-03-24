e2171e2e492bbe0bf06ce6dd7091e74b90f2d7f2
freddy_chura <freddychura93@gmail.com>
2024-03-24 18:44:20 -0400

    Implementando la interfaz OnInit para el componente producto

--- src/app/registro-productos/registro-productos.component.ts
+++ src/app/registro-productos/registro-productos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { ProductoService } from '../services/productos/producto.service';
 
 @Component({
   selector: 'app-registro-productos',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './registro-productos.component.html',
   styleUrl: './registro-productos.component.css'
 })
-export class RegistroProductosComponent {
+export class RegistroProductosComponent implements OnInit {
 
+  private productoService = inject(ProductoService);
+  productos: any[] = [];
+  ngOnInit(): void {
+      this.productoService.list()
+        .subscribe((productos: any) => {
+        this.productos = productos;
+      });
+
+    // this.productoForm = this.fb.group({
+    //     nombre : ['',Validators.required],
+    //     descripcion : ['',Validators.required],
+    //     precio : ['',Validators.required],
+    //     stock : ['',Validators.required],
+    //     urlimg : ['',Validators.required],
+
+    // })
+  }
+    guardar():void{
+  }
 }
