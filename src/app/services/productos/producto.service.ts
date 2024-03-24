f41ac3b344897aecee67f9ffd1b139f55bf0666c
freddy_chura <freddychura93@gmail.com>
2024-03-24 18:39:55 -0400

    Creando servicios de conexion para el componente producto

---
+++ src/app/services/productos/producto.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+  private http = inject(HttpClient);
+
+  list(){
+      return this.http.get('http://localhost:8080/productos');
+  }
+
+  get(id: number){
+    return this.http.get('http://localhost:8080/productos/${id}');
+  }
+
+  create(producto: any){
+    return this.http.post('http//localhost:8080/productos/',producto);
+
+  }
+  constructor() { }
+}
