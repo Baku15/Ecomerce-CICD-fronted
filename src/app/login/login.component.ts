0dd765a07e9bceaa4c7e39d28b535c9a6e0e1dc2
Alberto840 <alberto.flores@ucb.edu.bo>
2024-03-25 11:06:19 -0400

    generando html y ts para login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, RouterModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = ''; // Cambiamos para usar campos separados
+  password: string = '';
+  hide = true;
+  errorMessage: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogin(): void {
+    // Actualizamos para pasar username y password directamente
+  }
 }
