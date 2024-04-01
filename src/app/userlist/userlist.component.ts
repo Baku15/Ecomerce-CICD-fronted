5cb2efbc90fecef546e28e98ffed30bc3c9ecf6d
Alberto840 <alberto.flores@ucb.edu.bo>
2024-04-01 11:48:51 -0400

    agregando userlist con logica

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../services/Usuarios/usuario.service';
+import {MatTableModule} from '@angular/material/table';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-userlist',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './userlist.component.html',
   styleUrl: './userlist.component.css'
 })
-export class UserlistComponent {
+export class UserlistComponent implements OnInit {
+  dataSource = ELEMENT_DATA;
+  registrosUsers!: any;
+  usersFiltrados: any[] = this.registrosUsers;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'numero'];
+  constructor(public usuarioService: UsuarioService){}
+  ngOnInit(): void {
+    this.usuarioService.getAllUsuarios().subscribe(
+      response => {
+        this.registrosUsers = response;
+
+        // Manejar la respuesta de éxito aquí
+        console.log('Registros de users mostradas', response);
+      },
+      error => {
+        // Manejar el error aquí
+        console.error('Error al mostrar el users', error);
+      }
+    )
+    setTimeout(() => {    
+      this.usersFiltrados = this.registrosUsers;
+    }, 5000);
+  }
 
 }
