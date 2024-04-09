35441ecb458b6db8c03bf8b766c32da1cfcb10a2
Alberto840 <alberto.flores@ucb.edu.bo>
2024-04-09 12:17:02 -0400

    generando componente para detalle user

---
+++ src/app/detalleuser/detalleuser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalleuser',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalleuser.component.html',
+  styleUrl: './detalleuser.component.css'
+})
+export class DetalleuserComponent {
+
+}
