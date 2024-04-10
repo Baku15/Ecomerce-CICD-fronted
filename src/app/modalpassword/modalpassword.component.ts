c4d059ef9506aee44bfa18df7f1a94c663ec3a78
Alberto840 <alberto.flores@ucb.edu.bo>
2024-04-09 23:05:08 -0400

    generando modal para password

---
+++ src/app/modalpassword/modalpassword.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modalpassword',
+  standalone: true,
+  imports: [],
+  templateUrl: './modalpassword.component.html',
+  styleUrl: './modalpassword.component.css'
+})
+export class ModalpasswordComponent {
+
+}
