c9a4e3c0d6e75ef04d82d38478709a402b9488a9
freddy_chura <freddychura93@gmail.com>
2024-04-10 16:51:25 -0400

    creando interfaz para categoria producto

--- src/app/categoria-producto/categoria-producto.component.ts
+++ src/app/categoria-producto/categoria-producto.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from '../material-module';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria-producto',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule,ReactiveFormsModule],
   templateUrl: './categoria-producto.component.html',
   styleUrl: './categoria-producto.component.css'
 })
-export class CategoriaProductoComponent {
+export class CategoriaProductoComponent implements OnInit {
+categoriaForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.categoriaForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      estado: [true, [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.categoriaForm.valid) {
+      console.log(this.categoriaForm.value);
+    } else {
+      alert('Por favor complete el formulario correctamente');
+    }
+  }
 
 }
