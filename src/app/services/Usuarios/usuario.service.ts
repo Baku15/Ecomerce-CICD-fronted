e0234b7e3c29d179a38287afa4005b4e2253e197
Alberto840 <alberto.flores@ucb.edu.bo>
2024-04-25 23:49:13 -0400

    agregar servicio para login nuevo

--- src/app/services/Usuarios/usuario.service.ts
+++ src/app/services/Usuarios/usuario.service.ts
@@ -7,10 +7,12 @@ import { HttpClient } from '@angular/common/http'
 })
 export class UsuarioService {
 
-  private API_SERVER = "http://localhost:8081/api/v1/usuario";
+  private API_SERVER = "http://localhost:8050/api/user";
 
   constructor(private httpClient: HttpClient) { }
-
+  public login(credentials: any): Observable<any>{
+    return this.httpClient.post(this.API_SERVER+"/auth/login", credentials);
+  }
   public getAllUsuarios(): Observable<any>{
     return this.httpClient.get(this.API_SERVER);
   }
@@ -21,6 +23,15 @@ export class UsuarioService {
 
   public deleteUsuariobyId(id: number): Observable<any>{
     return this.httpClient.delete(this.API_SERVER + '/' + id);
+  }
+
+  //servicio para user
+  public registerNewUser(registroUsuario: any): Observable<any>{
+    return this.httpClient.post('http://localhost:8050/api/BusquedasP/createG', registroUsuario)
+  }
+  //servicio para persona
+  public registerNewPersona(registroUsuario: any): Observable<any>{
+    return this.httpClient.post(this.API_SERVER, registroUsuario)
   }
 }
 
