efcf7f3dc79049977b05bc6146e10d819d0f05ba
RobertoAlanoca <roberto.alanoca@ucb.edu.bo>
2024-04-26 00:56:55 -0400

    creado registro-carrito service

--- src/app/services/carritoCompras/carrito.service.ts
+++ src/app/services/carritoCompras/carrito.service.ts
@@ -12,17 +12,17 @@ export class CarritoService {
   private base_search = ''
   //get
   obtenerCarritoPorId(carritoId: string | number):Observable<any>{
-    return this.http.get<[]>(`http://localhost:8080/api/carrito-compras/buscar/${carritoId}`);
+    return this.http.get<[]>(`http://localhost:8095/api/carrito-compras/buscar/${carritoId}`);
   }
   //post un producto
   crearCarritoCompras(carritoDto: any){
-    return this.http.post('http://localhost:8080/api/carrito-compras',carritoDto);
+    return this.http.post('http://localhost:8095/api/carrito-compras',carritoDto);
   }
   obtenerTodosLosCarritos():Observable<any>{
-    return this.http.get('http://localhost:8080/api/carrito-compras/all');
+    return this.http.get('http://localhost:8095/api/carrito-compras/all');
   }
   eliminarCarritoPorId(carritoId: any):Observable<any>{
-    return this.http.delete(`http://localhost:8080/api/carrito-compras/eliminar/${carritoId}`);
+    return this.http.delete(`http://localhost:8095/api/carrito-compras/eliminar/${carritoId}`);
   }
 }
 
