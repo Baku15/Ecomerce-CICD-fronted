e7b885e1c55bcfdc6ef143bfa95c0ad2b13c273b
RobertoAlanoca <roberto.alanoca@ucb.edu.bo>
2024-06-05 17:34:29 -0400

    registro empleado modificacion css

--- src/app/registro-empleado/registro-empleado.component.ts
+++ src/app/registro-empleado/registro-empleado.component.ts
@@ -1,22 +1,33 @@
-import {Component} from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {merge} from 'rxjs';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { UsuarioService } from '../services/Usuarios/usuario.service';
 import { Router } from '@angular/router';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { merge } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-registro-empleado',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatDividerModule, MatIconModule,MatSelectModule,MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './registro-empleado.component.html',
-  styleUrl: './registro-empleado.component.css'
+  styleUrls: ['./registro-empleado.component.css']
 })
 export class RegistroEmpleadoComponent {
   hide = true;
@@ -33,13 +44,19 @@ export class RegistroEmpleadoComponent {
 
   errorMessage = '';
 
-  constructor(private usuarioService: UsuarioService,private router: Router,) {
+  constructor(private usuarioService: UsuarioService, private router: Router) {
     merge(this.email.statusChanges, this.email.valueChanges)
-      .pipe(takeUntilDestroyed())
+      .pipe(untilDestroyed(this))
       .subscribe(() => this.updateErrorMessage());
   }
-  registrarUser(){
-    if (this.nombre.value === '' || this.password.value === '' || this.telefono.value === '' || this.rol.value === '') {
+
+  registrarUser() {
+    if (
+      this.nombre.value === '' ||
+      this.password.value === '' ||
+      this.telefono.value === '' ||
+      this.rol.value === ''
+    ) {
       this.stringMessage = 'Debe llenar todos los campos';
       console.error('Debe llenar todos los campos');
       this.mostrarMensajeDeleteError();
@@ -54,23 +71,22 @@ export class RegistroEmpleadoComponent {
       password: this.password.value,
       username: this.username.value,
       roles: [this.rol.value]
-    }
+    };
     this.usuarioService.registerNewUser(this.newUser).subscribe({
       next: (response) => {
-        console.log(response+" Usuario registrado: "+this.newUser);
+        console.log(response + ' Usuario registrado: ' + this.newUser);
         this.mostrarMensajeDeleteExito();
-          setTimeout(() => {
-            this.router.navigate(['admin/userlist']);
-          }, 3000);
-        // Ajustamos según la respuesta real esperada
-        // Suponiendo que la respuesta contiene directamente los datos del usuario necesarios
+        setTimeout(() => {
+          this.router.navigate(['admin/userlist']);
+        }, 3000);
       },
       error: (error) => {
-        console.error(error+" Usuario registrado: "+this.newUser); // Para propósitos de depuración
+        console.error(error + ' Usuario registrado: ' + this.newUser);
         this.mostrarMensajeDeleteError();
       }
     });
   }
+
   updateErrorMessage() {
     if (this.email.hasError('required')) {
       this.errorMessage = 'You must enter a value';
@@ -80,42 +96,49 @@ export class RegistroEmpleadoComponent {
       this.errorMessage = '';
     }
   }
-  irhome(){
-    this.router.navigate(['admin/userlist']);
 
+  irhome() {
+    this.router.navigate(['admin/userlist']);
   }
+
   mostrarAlerta = false;
   mostrarAlertaError = false;
   mostrarAlertaDelete = false;
   mostrarAlertaErrorDelete = false;
+
   mostrarMensajeRegistroExito() {
     this.mostrarAlerta = true;
     setTimeout(() => {
       this.cerrarAlerta();
     }, 3000);
   }
+
   mostrarMensajeRegistroError() {
     this.mostrarAlertaError = true;
     setTimeout(() => {
       this.cerrarAlerta();
     }, 3000);
   }
+
   mostrarMensajeDeleteExito() {
     this.mostrarAlertaDelete = true;
     setTimeout(() => {
       this.cerrarAlerta();
     }, 3000);
   }
+
   mostrarMensajeDeleteError() {
     this.mostrarAlertaErrorDelete = true;
     setTimeout(() => {
       this.cerrarAlerta();
     }, 3000);
   }
+
   cerrarAlerta() {
     this.mostrarAlerta = false;
     this.mostrarAlertaError = false;
     this.mostrarAlertaDelete = false;
     this.mostrarAlertaErrorDelete = false;
   }
+  ngOnDestroy(): void {}
 }
